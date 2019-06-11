27aedb4e903a2e6f7b1e7652a2df25d9d48d1523
jwestcott9 <jwestcott9@gmail.com>
2019-06-11 12:52:41 -0600

    added comments

--- store.js
+++ store.js
@@ -1,11 +1,15 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require("cli-table");
+
+// setting some global variables so that they will be accessible globally
 var table;
 var price;
 var pruductSelected;
 var quantity;
 var quantityPurchased;
+var hasBought = false;
+var tableLength;
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -21,40 +25,49 @@ var connection = mysql.createConnection({
   password: "jcello69",
   database: "bamazon"
 });
-start();
 
-function start() {
-  connection.connect(function (err) {
-    if (err) throw err;
-    print();
-  })
-}
-var hasBought = false;
 
+// setting up the connection to mySQL
+connection.connect(function (err) {
+  if (err) throw err;
+  // this is the main start function declaration
+  print();
+})
+
+
+// this function will grab the data that is stored in the MYSQL database
+// it will then organize it into a table using the npm cli-table 
 function print() {
+  // this establishes the table and sets up how it will be organized
   table = new Table({
     head: ["ID", "Products", "Department", "Price", "Stock"],
     colWidths: [20, 20, 20, 20, 20]
   })
   console.log("Selecting all products...\n");
+  // this is where the code connects to mySQL and grabs all of the data from the products table
   connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
-    // Log all results of the SELECT statement
+    tableLength = res.length;
+    // Log all results of the SELECT statement and push it into the cli table
     for (var i = 0; i < res.length; i++) {
       table.push(
         [res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
       )
     }
-
+    // prints the table to the console
     console.log(table.toString());
+    // checks to see if the user just made a purchase if they have not run the buy function.
     if (!hasBought) {
       buy();
-    } else {
+    }
+    // if they have made a purchase thank them print the item they purchased and how much they spent and the table with the updated quantities
+    else {
       console.log(`
-Hey thanks for purchasing ${quantityPurchased} ${productSelected}s for $${price}!!
+Hey thanks for purchasing ${quantityPurchased} ${productSelected} for $${price}!!
 
 you're awesome
       `)
+      // calls a function that will send the user back through the prompt
       anotherPurchase();
 
 
@@ -64,7 +77,10 @@ you're awesome
   })
 
 };
-
+// this is the inquirer function.
+// this will ask the user to enter a product number that they are interested in
+// it will then ask the user how much they would like to purchase 
+// 
 function buy() {
   inquirer
     .prompt([{
@@ -77,45 +93,73 @@ function buy() {
       message: "how many would you like to buy?"
     }])
     .then(function (answer) {
-      connection.query("select * from products where ?", [{
-          id: parseInt(answer.whichProduct),
-        }],
-        function (err, res) {
-          
-          if (err) throw err;
+      // answer.which product will store the product id 
+      // answer.howMuch will store the amount that the user would like to purchase
+      var parseId = parseInt(answer.whichProduct);
+      var howMuch = parseInt(answer.howMuch);
+      // this will check to see if the id number entered is valid and is inside of the data table.
+      if (parseId <= tableLength) {
 
-          if (res[0].stock_quantity > answer.howMuch) {
-            price = res[0].price * answer.howMuch;
-            productSelected = res[0].product_name;
-            quantityPurchased = answer.howMuch;
-            quantity = res[0].stock_quantity - answer.howMuch;
-            console.log(`The total price of your purchase will be $${price}`)
-            inquirer.prompt(
-              [{
-                type: "confirm",
-                name: "priceConfirm",
-                message: "would you like to make this purchase?"
-              }]
-            ).then(function (confirmation) {
-              if (confirmation.priceConfirm === true) {
-               
-                decrementAndPrint(answer.whichProduct, quantity);
-              }
-            })
+        // connects with the database and grabs the data that is related to the id number entered.
+        connection.query("select * from products where ?", [{
+            id: parseId
+          }],
+          function (err, res) {
+            var stock = res[0].stock_quantity;
+            if (err) throw err;
 
-          } else {
-            console.log("There is insufficient stock to make this purchase")
-            anotherPurchase();
-          }
-        })
+            // this if statement will check to see if there is enough stock to fulfill the user request
+            if (stock > howMuch) {
+              // all this will take the data points from the response and save them in global variables
+              price = res[0].price * howMuch;
+              productSelected = res[0].product_name;
+              quantityPurchased = howMuch;
+              quantity = res[0].stock_quantity - howMuch;
+
+              // this will check with the user if the price and quantity is correct and ask them to confirm 
+              // if they confirm it will call a function that will thank them and ask if they would like to buy something more
+              console.log(`You have selected ${howMuch} ${productSelected} - The total price of your purchase will be $${price}`)
+              inquirer.prompt(
+                [{
+                  type: "confirm",
+                  name: "priceConfirm",
+                  message: "would you like to make this purchase?"
+                }]
+              ).then(function (confirmation) {
+                if (confirmation.priceConfirm === true) {
+                  
+                  // calls a function that will handle the update and decrement of quantity and also redirect the user
+                  // to the anotherPurchase function
+                  decrementAndPrint(parseId, quantity);
+                }
+              })
+
+            }
+            // this is the else that corresponds to the check to for stock quantity
+            else {
+              console.log("There is insufficient stock to make this purchase")
+              anotherPurchase();
+            }
+          })
+      }
+      //  this is the else that corresponds to checking if the product id exists
+      else {
+        console.log("that was an invalid product number");
+        anotherPurchase();
+
+      }
     })
 }
 
+
+// called after the user has made a purchase 
+// called if the user failed to enter a valid product id
+// or if the user changes their mind about a purchase
 function anotherPurchase() {
 
   inquirer.prompt([{
     name: "another",
-    message: "would you like to make a different purchase?",
+    message: "would you like to make another purchase?",
     type: "confirm",
 
   }]).then(function (answer) {
@@ -131,9 +175,9 @@ function anotherPurchase() {
 }
 
 
-
+// here all of the logic for updating quantities in the database is handled
 function decrementAndPrint(answer, quantity) {
-console.log(answer);
+  console.log(answer);
   connection.query(
     "UPDATE products SET ? WHERE ?",
     [{
